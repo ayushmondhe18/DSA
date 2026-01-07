216745611438c7dfd268407f98b74f7a442420ce
ayushmondhe18 <ayushmondhe@gmail.com>
2026-01-07 11:54:03 -0500

    Add files via upload

---
+++ recursion/FibonacciNo.java
@@ -0,0 +1,12 @@
+public class FibonacciNo {
+    public static int fib(int n){
+        if(n == 1 || n == 0){
+            return n ;
+        }
+        int fib =fib(n-1) + fib(n-2);
+        return fib;
+    }
+    public static void main(String[] args) {
+        System.out.println(fib(10));
+    }
+}
